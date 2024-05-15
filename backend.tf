77ec657bc711d9f42c211ee4e68e975f09fd2f59
Asaph Tejou Danchi <112729006+asaphdanchi@users.noreply.github.com>
2024-05-15 02:05:58 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "vamsi-chunduru-923874"
+    bucket = "solavise-tech-4535"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
     workspace_key_prefix  = "env"
